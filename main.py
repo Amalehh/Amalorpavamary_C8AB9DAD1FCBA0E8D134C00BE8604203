c41983e22382ee318547515e1081c99bbf7035a4
Amalehh <144368076+Amalehh@users.noreply.github.com>
2023-09-07 21:43:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Factorial")
+n = int(input("Enter a number "))
+
+
+def factorial(n):
+  if n == 1:
+    return 1
+  else:
+    return (n * factorial(n - 1))
+
+
+print("Factorial of", n, "is", factorial(n))
